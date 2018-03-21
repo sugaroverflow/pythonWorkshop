c1948df683d0163c73e7c3a10ba602d09d146e4a
Fatima Khalid <sugaroverflow@gmail.com>
2018-03-20 21:55:08 -0400

    first commit.

---
+++ index.md
@@ -0,0 +1,37 @@
+Interactive Intro to _Python_
+
+---
+Hi I'm @sugaroverflow
+
+* Tech Lead at Digital Echidna
+* BS Computer Science | NYU Engineering
+
+---
+Introduction to Python
+
+---
+Overview
+
+---
+Python Libraries we'll cover today
+
+---
+urllib2
+
+---
+beautiful soup
+
+---
+wunderground api
+
+---
+calling the api
+
+---
+getting the data
+
+---
+what you can do with the data
+
+---
+matplotlib
---
+++ introscript.py
@@ -0,0 +1,16 @@
+import urllib2
+import json
+from keys import WAPI_KEY
+
+f = urllib2.urlopen('http://api.wunderground.com/api/0def10027afaebb7/geolookup/conditions/q/IA/Cedar_Rapids.json')
+json_string = f.read()
+
+parsed_json = json.loads(json_string)
+
+location = parsed_json['location']['city']
+
+temp_f = parsed_json['current_observation']['temp_f']
+
+print "Current temperature in %s is: %s" % (location, temp_f)
+
+f.close()
